a6b0510c499eb119605861c93b129e485c7c87c3
Muhammad Brilian Erranaomi <brilianeranaomi@gmail.com>
2022-09-30 06:48:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# beauty-dashboard
+
+link color
+https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=ffe0b2
+
+This template should help get you started developing with Vue 3 in Vite.s
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div class="card" style="width: 20rem;">
+      <img src="./assets/img/AkNo-logos_transparent.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <div class="form-floating mb-3">
+          <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="d-grid">
+          <button class="btn btn-primary" type="submit">Login</button>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('submit', this.email)
+    }
+  }
+}
+</script>
